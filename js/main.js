33113f1f38f89e955ffa607b8b5c4b41ef93ba7d
zLaine <rumtehk_uh@yahoo.com>
2015-02-18 04:09:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = function()
     
     "use strict";
     
-    var game = new Phaser.Game(3200, 1824, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
 
     var people;
     var dragon;
@@ -26,7 +26,7 @@ window.onload = function()
         game.load.image('grass', 'assets/grass.png');
         game.load.image('BG', 'assets/grassyBG.png');
         
-        game.load.audio('reunited', 'assets/Reunited1.mp3');
+        game.load.audio('reunited', 'assets/Reunited.mp3');
     }
     
     
