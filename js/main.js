546c5866384c057135727436f89ae76c3c682ffa
zLaine <rumtehk_uh@yahoo.com>
2015-02-18 04:05:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,10 +43,11 @@ window.onload = function()
         reunited.play();
         
         dragon = game.add.sprite(32, game.world.height - 150, 'sindra');
-        dragon.physics.arcade.enable(dragon);
+        game.physics.arcade.enable(dragon);
         dragon.body.bounce.y = 0.2;
         dragon.body.gravity.y = 1000;
         dragon.anchor.setTo(.5,.5);
+        game.camera.follow(dragon);
         dragon.body.collideWorldBounds = true;
         //dragon.scale.set(2);
         
