f8323a1465231e8531f34fcf8b65b667772a94e1
zLaine <rumtehk_uh@yahoo.com>
2015-02-20 05:28:01 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -92,8 +92,8 @@ window.onload = function()
         flap.volume = .5;
         flap.onDown.add(flapWait, this);
         
-        style = { font: "10px Arial", fill: "#000000", align: "center" };
-        goal = game.add.text(16, 16, 'Look around, see if you can see any \nclues that might help you find your hatchling.');
+        style = { font: "10px Arial", fill: "#ffffff", align: "center" };
+        goal = game.add.text(16, 16, 'Look around, see if you can see any \nclues that might help you find your hatchling.', style);
         goal.fixedToCamera = true;
         
         game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
