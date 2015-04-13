153e7721e6289ffbeec121a418be145a8515477e
zLaine <rumtehk_uh@yahoo.com>
2015-04-13 11:54:56 -0400

    Create IntroState.js

---
+++ js/IntroState.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function IntroState(){};
+
+IntroState.prototype =
+{
+    //create intro sprite
+    create: function()
+    {
+        console.log("Intro");
+        
+        var startScreen = this.game.add.sprite(0,0, 'startScreen');
+        
+        //set the start variables for the game
+        
+        this.game.level = 0;
+	    this.game.score = 0;
+        
+    },
+    
+    update: function()
+    {
+        //wait for click input go to play
+        if(this.game.input.activePointer.isDown)
+        {
+            this.game.state.start("play");
+        }
+    }
+};
