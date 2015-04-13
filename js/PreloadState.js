b76949c52a7fdbd0bffed8a4ef5ecf61c597de0b
zLaine <rumtehk_uh@yahoo.com>
2015-04-13 11:51:47 -0400

    Update PreloadState.js

--- js/PreloadState.js
+++ js/PreloadState.js
@@ -13,19 +13,25 @@ PreloadState.prototype =
             this.load.setPreloadSprite(loadingBar); */
             
         /* Load tile map */
+        this.game.load.tilemap('map', 'assets/flightForest.json', null, Phaser.Tilemap.TILED_JSON);
         
         /* Load images */
         this.game.load.image("startScreen", "assets/startScreen.png");
         this.game.load.image("victoryScreen", "assets/victoryScreen.png");
+        
+        this.game.load.image('footPrints', 'assets/prints.png');
+        this.game.load.image('treeTops', 'assets/aboveTrees.png');
+        this.game.load.image('bloodSmell', 'assets/blood.png');
+        this.game.load.image('townView', 'assets/view.png');
+        this.game.load.image('forest', 'assets/forest_background_by_jbjdesigns-d5mgjm3.png');
 
         /* Load spritesheets */
-        this.game.load.spritesheet("bubbles", "assets/bubbles.png", 40, 40);
+        this.game.load.spritesheet('sindra', 'assets/dragonProtag1.png', 200, 150, 52);
             
         /* Load sounds */
-        this.game.load.audio("pop1", "assets/pop1.mp3");
-        this.game.load.audio("pop2", "assets/pop2.mp3");
-        this.game.load.audio("pop3", "assets/pop3.mp3");
-        this.game.load.audio("bgm", "assets/Reunited.mp3");
+        this.game.load.audio('reunited', 'assets/Reunited.mp3');
+        this.game.load.audio('flapping', 'assets/flapFast.mp3');
+        this.game.load.audio('bgm', 'assets/DeathIsJustAnotherPath.mp3');
     },
     
     create: function()
