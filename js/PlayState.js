d8bf70940847d9a53473fe6b00322a562a55599e
zLaine <rumtehk_uh@yahoo.com>
2015-04-16 02:23:44 -0400

    Update PlayState.js

--- js/PlayState.js
+++ js/PlayState.js
@@ -226,12 +226,12 @@ PlayState.prototype =
             if(this.dragon.body.velocity.y < 0)
             {
                 //this.dragon.body.velocity.y += (this.flapHeight);
-                this.dragon.body.velocity.y -= 50;
+                this.dragon.body.velocity.y -= 100;
             }
             else
             {
                 //this.dragon.body.velocity.y += this.flapHeight;
-                this.dragon.body.velocity.y -= 50;
+                this.dragon.body.velocity.y -= 100;
             }
             //this.dragon.body.velocity.y = this.flapHeight;
             
